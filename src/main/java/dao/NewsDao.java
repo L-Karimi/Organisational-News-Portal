eca8ee2563db228261d0b0ee62483fde1e3bbd90
luciahroyalty101 <luciahkei101@gmail.com>
2020-12-14 07:27:47 +0300

    defination of the basic dao layer api

--- src/main/java/dao/NewsDao.java
+++ src/main/java/dao/NewsDao.java
@@ -1,4 +1,24 @@
 package dao;
+import models.*;
+import java.util.List;
 
 public interface NewsDao {
+
+    void add(News news);
+    void addNewsToDepartment(News news, DepartmentDao.Department department);
+
+
+    List<News> getAll();
+    News findById(int id);
+    List<News> getAllNewsByDepartment(int departmentId);
+
+    List<News> getNewsByDepartment(int departmentId);
+
+
+
+    void update(int id, String newsTitle, String newsContent,int departmentId);
+
+    void deleteById(int id);
+    void clearAll();
+
 }
