1ebbb1e8b5b7c88da4349969c1fd8719deb8b358
luciahroyalty101 <luciahkei101@gmail.com>
2020-12-14 08:13:30 +0300

    implement method to add user

--- src/main/java/dao/Sql2oUserDao.java
+++ src/main/java/dao/Sql2oUserDao.java
@@ -1,17 +1,61 @@
 package dao;
-
+import models.News;
 import models.User;
+
+import java.util.ArrayList;
+import java.util.List;
 import org.sql2o.Sql2o;
+import org.sql2o.Connection;
+import org.sql2o.Sql2oException;
 
-public class Sql2oUserDao {
+public class Sql2oUserDao implements UserDao{
+
+    private final Sql2o sql2o;
+
     public Sql2oUserDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add(User user) {
+        String sql = "INSERT INTO users (userName, userCompanyPosition, userCompanyRole, departmentId) VALUES (:userName, :userCompanyPosition, :userCompanyRole, :departmentId)";
+        try(Connection con = sql2o.open()){
+            int id = (int) con.createQuery(sql, true)
+                    .bind(user)
+                    .executeUpdate()
+                    .getKey();
+            user.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return null;
+    }
+
+    @Override
+    public User findUserById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAllUsersByDepartment(int departmentId) {
+        return null;
+    }
+
+    @Override
+    public void update(int id, String userName, String userCompanyPosition, String useCompanyRole, int departmentId) {
 
     }
 
-    public void add(User testUser) {
+    @Override
+    public void deleteById(int id) {
 
     }
 
+    @Override
     public void clearAll() {
 
     }
--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -8,4 +8,8 @@ public class User {
     public int getId() {
         return 0;
     }
+
+    public void setId(int id) {
+
+    }
 }
