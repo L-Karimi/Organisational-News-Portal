d7f5ebf96d1db5cf2998d7d4ee8e1d5828a5a228
luciahroyalty101 <luciahkei101@gmail.com>
2020-12-14 08:07:59 +0300

    declare use methods

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,22 @@
 package dao;
+import models.*;
+import java.util.List;
 
 public interface UserDao {
+
+
+    void add (User user);
+
+
+    List<User> getAllUsers();
+    User findUserById(int id);
+    List<User> getAllUsersByDepartment(int departmentId);
+
+
+    void update(int id, String userName,  String userCompanyPosition, String useCompanyRole, int departmentId);
+
+
+    void deleteById(int id);
+    void clearAll();
+
 }
