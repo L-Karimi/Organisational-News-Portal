1f10d755a4bceb8b5e089e2c76cadb0b969ed96b
luciahroyalty101 <luciahkei101@gmail.com>
2020-12-14 09:31:16 +0300

    create constructors for department

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,4 +1,29 @@
 package models;
 
+import java.util.Objects;
+
 public class Department {
+    private String departmentName;
+    private String departmentDescription;
+    private int departmentEmployeesNumber;
+    private int id;
+
+
+    public Department(String departmentName, String departmentDescription, int departmentEmployeesNumber) {
+        this.departmentName = departmentName;
+        this.departmentDescription = departmentDescription;
+        this.departmentEmployeesNumber = departmentEmployeesNumber;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return departmentEmployeesNumber == that.departmentEmployeesNumber &&
+                id == that.id &&
+                departmentName.equals(that.departmentName) &&
+                departmentDescription.equals(that.departmentDescription);
+    }
 }
